956fd418183adad63a439e6fe8ca2b1a40f7ff69
Patrick Feeley <pgf8@case.edu>
2015-03-03 18:48:18 -0500

    Thread closes with window

--- main.py
+++ main.py
@@ -15,17 +15,25 @@ alt = tk.Label(root, textvariable=alt_val).pack
 rot = tk.Label(root, textvariable=rot_val).pack
 acc = tk.Label(root, textvariable=acc_val).pack
 
+alive = True
+
 def run():
-  print getData.read()
-  alt_val.set("0.00 m")
-  rot_val.set("0.00 d/s")
-  acc_val.set("0.00 m/s")
-  time.sleep(1)
-
-def data():
   while True:
-    run()
+    print getData.read()
+    alt_val.set("0.00 m")
+    rot_val.set("0.00 d/s")
+    acc_val.set("0.00 m/s")
+    time.sleep(1)
+    if alive == False:
+      raise Exception("Window Closed...")
 
-thread = Thread(target=data)
+thread = Thread(target=run)
+
+def handler():
+  alive = False
+  root.quit()
+
+root.protocol("WM_DELETE_WINDOW", handler)
+
 thread.start()
 root.mainloop()
