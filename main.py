0ac4b68e91222cd7eb4333f5c74f8cbd94a447bc
Patrick Feeley <pgf8@case.edu>
2015-03-03 19:16:58 -0500

    Add content to the GUI window

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Tkinter as tk
 import time
 import getData
+import sendData
 from threading import Thread
 
 root = tk.Tk()
@@ -11,9 +12,21 @@ alt_val = tk.StringVar()
 rot_val = tk.StringVar()
 acc_val = tk.StringVar()
 
-alt = tk.Label(root, textvariable=alt_val).pack
-rot = tk.Label(root, textvariable=rot_val).pack
-acc = tk.Label(root, textvariable=acc_val).pack
+alt_label = tk.Label(root, text="Altitude:    ").grid(row=0, column=0)
+rot_label = tk.Label(root, text="Rotation:    ").grid(row=1, column=0)
+acc_label = tk.Label(root, text="Acceleration:").grid(row=2, column=0)
+
+alt = tk.Label(root, textvariable=alt_val).grid(row=0, column=1)
+rot = tk.Label(root, textvariable=rot_val).grid(row=1, column=1)
+acc = tk.Label(root, textvariable=acc_val).grid(row=2, column=1)
+
+def gps_callback():
+  sendData.start_gps()
+
+gps = tk.Button(root, text="Connect GPS", command=gps_callback).grid(row=0, column=3)
+
+def gps_callback():
+  sendData.start_gps()
 
 alive = True
 
---
+++ sendData.py
@@ -0,0 +1,3 @@
+def start_gps():
+  print("Starting GPS")
+  # TODO: Put the code to acquire signal here
